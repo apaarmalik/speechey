bab376adfc8c71a4f870729f237f658d9106a313
apaarmalik <144600507+apaarmalik@users.noreply.github.com>
2024-11-07 00:10:09 +0530

    app.py

--- app.py
+++ app.py
@@ -1,16 +1,18 @@
-import streamlit as st
-import speech_recognition as sr
+import speech_recognition
+import pyttsx3
 
-container=st.container()
-recognizer=sr.Recognizer()
-recognizer.energy_threshold=300
+recognizer = speech_recognition.Recognizer()
 
-mic=sr.Microphone()
-with mic as source:
-    st.write("say something")
-    audio=recognizer.listen(source)
+while True:
     try:
-        text=recognizer.recognize_google(audio)
-        st.write(text)
-    except sr.UnknownValueError:
-        st.write("could not understand")
+        with speech_recognition.Microphone() as mic:
+            recognizer.adjust_for_ambient_noise(mic, duration=0.2)
+            audio = recognizer.listen(mic)
+            
+            text = recognizer.recognize_google(audio)
+            text = text.lower()
+            print(text)
+            
+    except:
+        recognizer = speech_recognition.Recognizer()
+        continue
