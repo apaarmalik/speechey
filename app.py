84a67e6eee27b27f66080934e0d40d1ff49a52c5
apaarmalik <144600507+apaarmalik@users.noreply.github.com>
2024-11-07 00:32:11 +0530

    app.py

--- app.py
+++ app.py
@@ -1,18 +1,18 @@
-import speech_recognition
-import pyttsx3
+import speech_recognition as sr 
+#import playsound # to play saved mp3 file
+#from gtts import gTTS # google text to speech
+#import os # to save/open files
+#import wolframalpha # to calculate strings into formula
+#from selenium import webdriver
 
-recognizer = speech_recognition.Recognizer()
-
-while True:
-    try:
-        with speech_recognition.Microphone() as mic:
-            recognizer.adjust_for_ambient_noise(mic, duration=0.2)
-            audio = recognizer.listen(mic)
-            
-            text = recognizer.recognize_google(audio)
-            text = text.lower()
-            print(text)
-            
-    except:
-        recognizer = speech_recognition.Recognizer()
-        continue
+rObject = sr.Recognizer()
+audio = ''
+ 
+with sr.Microphone() as source:
+    print("Speak...")
+         
+        # recording the audio using speech recognition\
+    audio = rObject.listen(source, phrase_time_limit = 5) 
+    text = rObject.recognize_google(audio, language ='en-US')
+    print("You : ", text)
+    print("Stop.")
